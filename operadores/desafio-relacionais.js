23469073cfeaae0de32caea9680971e5174c89b7
Luiz Roberto Vieira da Silva Filho <147288001+Lrfilho@users.noreply.github.com>
2025-03-08 23:39:27 +0000

    Atualização operadores

---
+++ operadores/desafio-relacionais.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 2;
+
+// TRUE
+console.log(a < b);
+console.log(a != b);
+console.log(a <= b);
+console.log(b > a);
+console.log(b >= a);
+
+
+// FALSE
+console.log(a > b);
+console.log(a >= b);
+console.log(a == b);
+console.log(b < a);
+console.log(b === a);
---
+++ operadores/desafio_atribuicao.js
@@ -0,0 +1,8 @@
+let texto = "Lista de Aprovados \n==================\n";
+texto += "\n1. Ana Silva\n";
+texto += "2. Pedro Albuquerque\n";
+texto += "3. Guilherme Pereira\n";
+texto += "4. Rebeca França";
+
+
+console.log(texto)
