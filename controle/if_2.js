3b55e76b1d61b9b7a58fe91b6e036ca3ff76e9fa
Luiz Roberto Vieira da Silva Filho <147288001+Lrfilho@users.noreply.github.com>
2025-03-10 23:14:42 +0000

    Inicio de Controle

---
+++ controle/if_2.js
@@ -0,0 +1,11 @@
+const nota = 10;
+
+
+if (nota >=7 && nota != 10) 
+    console.log('Parabéns!')
+
+if (nota < 7 )
+    console.log('Não foi dessa vez, tente de novo!')
+
+if (nota == 10)
+    console.log('PARABÉNS...TIOU A NOTA MAXIMA!')
---
+++ controle/if_3.js
@@ -0,0 +1,15 @@
+const nota = 3.4;
+
+
+
+if (nota >= 9)
+   {
+
+    console.log('Quadro de honra ')
+    console.log('Parabéns')
+
+
+
+   } 
+
+
