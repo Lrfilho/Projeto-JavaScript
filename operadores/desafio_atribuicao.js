ab6d4bcd81dd077dd0debe6a0f7572f05ee6fc9f
Luiz Roberto Vieira da Silva Filho <147288001+Lrfilho@users.noreply.github.com>
2025-03-10 19:19:13 +0000

    Atualização operadores

--- operadores/desafio_atribuicao.js
+++ operadores/desafio_atribuicao.js
@@ -1,5 +1,6 @@
-let texto = "Lista de Aprovados \n==================\n";
-texto += "\n1. Ana Silva\n";
+let texto = "Lista de Aprovados \n";
+texto += "==================\n\n"
+texto += "1. Ana Silva\n";
 texto += "2. Pedro Albuquerque\n";
 texto += "3. Guilherme Pereira\n";
 texto += "4. Rebeca França";
---
+++ operadores/desafio_logicos.js
@@ -0,0 +1,13 @@
+// Duas expressões resultar em TRUE
+let verdadeira1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeira2 = 10 - 6 <= 5 || 20 < 19; 
+
+console.log(verdadeira1)
+console.log(verdadeira2)
+
+// Duas expressões resultar em FALSE
+let falso1 = "1" === 1 || 25 < 24;
+let falso2 = !(19*5 > 30)
+
+console.log(falso1)
+console.log(falso2)
