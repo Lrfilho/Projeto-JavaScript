622fb458465a4d38a6fa9a47340a93a0dc605b02
Luiz Roberto Vieira da Silva Filho <147288001+Lrfilho@users.noreply.github.com>
2025-04-08 00:23:13 +0100

    ..

---
+++ Funções/funcao_como_var.js
@@ -0,0 +1,15 @@
+const x = 4;
+let y = 5;
+var w = 2;
+
+const somar  = function(a, b){
+    return a + b;
+
+}
+
+const diminuir = function(c, d){
+    return c - d;
+}
+
+console.log(somar(x, y));
+console.log(diminuir(x, w))
