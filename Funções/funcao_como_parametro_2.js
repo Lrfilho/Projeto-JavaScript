5567dff6a20ebd1c1a591c6d432e8cd10dac0590
Luiz Roberto Vieira da Silva Filho <147288001+Lrfilho@users.noreply.github.com>
2025-04-14 00:41:08 +0100

    atualização

--- Funções/funcao_como_parametro_2.js
+++ Funções/funcao_como_parametro_2.js
@@ -1,6 +1,6 @@
 function executar(fn) {
     if(typeof fn === "function") {
-        console.log(fn());
+        console.log(fn(90, 40));
     }
 }
 
@@ -9,4 +9,19 @@ function somar (a, b) {
     return a + b;
 }
 
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
 executar(somar);
+executar(subtrair);
+executar(multiplicar);
+executar(dividir);
