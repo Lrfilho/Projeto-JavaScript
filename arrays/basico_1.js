ce9eb5996423b149771b2dfedc2e078e16e03b90
Luiz Roberto Vieira da Silva Filho <147288001+Lrfilho@users.noreply.github.com>
2025-04-21 23:18:54 +0100

    .

---
+++ arrays/basico_1.js
@@ -0,0 +1,8 @@
+//              0    1    2    3    4    5
+const notas = [7.8, 9.5, 4.9, 8.1, 7.6, 9.8];
+
+
+console.log(notas);
+console.log (notas[0]);
+console.log (notas[3]);
+console.log (notas[5]);
